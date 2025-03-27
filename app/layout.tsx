38afde5b2052af822a3f417efd97f7b27cf9bc15
Elias Ripari <eliasripari@gmail.com>
2025-03-27 17:20:51 +0400

    Update package dependencies by adding @react-spring and ogl libraries, enhance layout with new Aurora component in the footer, and improve styling for better user experience

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,7 @@ import Image from "next/image";
 import Logo from "@/public/logo-fleetmo.svg";
 import Balancer from "react-wrap-balancer";
 import AnimatedContent from "@/components/AnimatedContent/AnimatedContent";
+import Aurora from "@/components/Aurora/Aurora";
 
 import { cn } from "@/lib/utils";
 
@@ -125,8 +126,16 @@ const Nav = ({ className, children, id }: NavProps) => {
 const Footer = () => {
   return (
     <footer>
-      <Section className="bg-foreground !-mt-12 border-t border-white/10 !p-12 ">
-        <Container className="!p-0">
+      <Section className="bg-foreground !-mt-12 border-t border-white/10  !p-0 !bg-black">
+        <div className="w-full min-h-[200px]">
+          <Aurora
+            colorStops={["#3A29FF", "#FF94B4", "#FF3232"]}
+            blend={3.5}
+            amplitude={1}
+            speed={0.5}
+          />
+        </div>
+        <div className="w-full max-w-7xl mx-auto p-8">
           <Image
             src={Logo}
             alt="Logo"
@@ -135,15 +144,15 @@ const Footer = () => {
             width={1920}
             height={1080}
           ></Image>
-        </Container>
-        <Container className="grid md:grid-cols-[1.5fr_0.5fr_0.5fr] gap-12 text-white !p-0 !py-12">
+        </div>
+        <Container className="grid md:grid-cols-[1.5fr_0.5fr_0.5fr] gap-12 text-white px-8">
           <div className="flex flex-col gap-6 not-prose">
             <p>
               <Balancer>{siteConfig.site_description}</Balancer>
             </p>
           </div>
           <div className="flex flex-col gap-2 text-sm">
-            <h5 className="font-medium text-base">Website</h5>
+            <h5 className="font-medium text-base">Links</h5>
             {Object.entries(mainMenu).map(([key, href]) => (
               <Link
                 className="hover:underline underline-offset-4"
@@ -155,7 +164,7 @@ const Footer = () => {
             ))}
           </div>
           <div className="flex flex-col gap-2 text-sm">
-            <h5 className="font-medium text-base">Blog</h5>
+            {/* <h5 className="font-medium text-base">Blog</h5>
             {Object.entries(contentMenu).map(([key, href]) => (
               <Link
                 className="hover:underline underline-offset-4"
@@ -164,16 +173,18 @@ const Footer = () => {
               >
                 {key.charAt(0).toUpperCase() + key.slice(1)}
               </Link>
-            ))}
+            ))} */}
           </div>
         </Container>
-        <Container className="border-t border-white/10 not-prose flex flex-col md:flex-row md:gap-2 gap-6 justify-between md:items-center !container !px-0">
+        <Container className="not-prose flex flex-col md:flex-row md:gap-2 gap-6 justify-between md:items-center p-8">
           {/* <ThemeToggle /> */}
-          <p className="text-muted-foreground">
-            &copy; <a href="https://9d8.dev">{siteConfig.site_name}</a> -
-            Service offered by Wonder Rides FZCO - All rights reserved -
-            2025-present.
-          </p>
+          <div className="border-t border-white/10 w-full py-8">
+            <p className="text-muted-foreground">
+              &copy; <a href="https://9d8.dev">{siteConfig.site_name}</a> -
+              Service offered by Wonder Rides FZCO - All rights reserved -
+              2025-present.
+            </p>
+          </div>
         </Container>
       </Section>
     </footer>
